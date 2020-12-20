d5b8eab86a5b0488afa157ff2196a70fe35380b1
abhinavraj23 <abhinav.23.april@gmail.com>
2020-12-20 16:31:16 +0530

    feat: add signUp API

--- splitit/api/views.py
+++ splitit/api/views.py
@@ -16,7 +16,6 @@ from django.utils import timezone
 from datetime import datetime
 
 from api.models import *
-from api.documents import BookingDocument
 
 import sys
 import logging
@@ -28,3 +27,37 @@ import math
 import random
 
 logger = logging.getLogger(__name__)
+
+
+class SignUpAPI(APIView):
+
+    def post(self, request, *args, **kwargs):
+        response = {}
+        resp_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+        try:
+            data = request.data
+            logger.info("SignUpAPI: %s", str(data))
+            if not isinstance(data, dict):
+                data = json.loads(data)
+
+            first_name = data["first_name"]
+            last_name = data["last_name"]
+            email = data["email"]
+            password = data["password"]
+            amount_owed = 0
+
+            if SplititUser.objects.filter(username=email).exists() == False:
+                splitit_user_obj = SplititUser.objects.create(
+                    username=email, first_name=first_name, last_name=last_name, email=email, amount_owed=amount_owed)
+                splitit_user_obj.set_password(password)
+                splitit_user_obj.save()
+                response["username"] = splitit_user_obj.username
+                response["status"] = 200
+            else:
+                response["status"] = 409
+
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            logger.error("SignUpAPI: %s at %s", e, str(exc_tb.tb_lineno))
+
+        return Response(data=response, status=resp_status)
